0a9dd9f160163659f7c0536cbbedc7e4343369a8
Erik Dubois <erik.dubois@gmail.com>
2020-10-20 15:58:31 +0200

    comparison with arcolinux system config

---
+++ usr/local/bin/arcolinux-appstream-data-fix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#echo "Fix for icons not showing up in pamac-aur"
+
+zcat /usr/share/app-info/xmls/community.xml.gz | sed 's|<em>||g;s|<\/em>||g;' | gzip > "new.xml.gz"
+mv new.xml.gz /usr/share/app-info/xmls/community.xml.gz
+appstreamcli refresh-cache --force
